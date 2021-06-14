4d41781e338315ce6336522aad893c5ea9c9ef06
thanhnc <thanhnc138@mail.com>
2021-06-14 23:14:55 +0700

    test

--- application/api/users/UserController.py
+++ application/api/users/UserController.py
@@ -1,26 +1,26 @@
 from flask import Blueprint
 from flask_restful import Api
 from application.api.users.User import User
-from application.api.users.UserSchema import user_schema, users_schema, users_paging_schema
-from application.api.base.BaseController import BaseController, BaseListController
+# from application.api.users.UserSchema import user_schema, users_schema, users_paging_schema
+# from application.api.base.BaseController import BaseController, BaseListController
 
 user_api = Blueprint("user_api", __name__, url_prefix='/api/users')
 api = Api(user_api)
 
 
-class UserController(BaseController):
-    model = User
-    schema = user_schema
+# class UserController(BaseController):
+#     model = User
+#     schema = user_schema
 
 
-api.add_resource(UserController, '/<string:id>', endpoint='user')
+# api.add_resource(UserController, '/<string:id>', endpoint='user')
 
 
-class UserListController(BaseListController):
-    model = User
-    schema = user_schema
-    list_schema = users_schema
-    paging_schema = users_paging_schema
+# class UserListController(BaseListController):
+#     model = User
+#     schema = user_schema
+#     list_schema = users_schema
+#     paging_schema = users_paging_schema
 
 
-api.add_resource(UserListController, '', endpoint='users')
+# api.add_resource(UserListController, '', endpoint='users')
